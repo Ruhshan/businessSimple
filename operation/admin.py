6c687a3302ac99835ea689393ba50daa484cff76
Ruhshan <ruhshan.ahmed@gmail.com>
2020-07-16 11:03:32 +0600

    Add issue date in Daily summary admin list view

--- operation/admin.py
+++ operation/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from operation.models import DailySummary
 
 class DailySummaryAdmin(admin.ModelAdmin):
-    list_display = ['code', 'date','stockStart','stockEnd','totalReceived']
+    list_display = ['code', 'date','stockStart','stockEnd','totalReceived','totalIssued']
 
 admin.site.register(DailySummary, DailySummaryAdmin)
 # Register your models here.
