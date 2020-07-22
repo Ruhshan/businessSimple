df1225686b2bb944d86e902c4b27c7cd3517a2bb
Ruhshan <ruhshan.ahmed@gmail.com>
2020-07-22 16:35:46 +0600

    Enable issue validation with respect to daily summary

--- operation/forms/issue_form.py
+++ operation/forms/issue_form.py
@@ -1,15 +1,36 @@
 from django.forms import ModelForm, ValidationError
-from operation.models import Issue
+from operation.models import Issue, DailySummary
 import datetime
+from django.utils.translation import ugettext_lazy as _
+
 
 class IssueForm(ModelForm):
     def clean_date(self):
         today = datetime.datetime.today().date()
         form_date = self.cleaned_data['date']
+        form_product = self.cleaned_data['product']
+        has_previous_summary = DailySummary.objects.has_previous_summary(form_product, form_date)
+
         if today < form_date:
             raise ValidationError("Future date is not allowed")
+
+        if not has_previous_summary:
+            raise ValidationError("No product in stock for this date")
         return form_date
+
+    def clean_unit(self):
+        if len(self.errors.keys()) == 0:
+            form_date = self.cleaned_data['date']
+            form_product = self.cleaned_data['product']
+            summary_for_date = DailySummary.objects.get_summary_for_date(form_product, form_date)
+            if summary_for_date.stockEnd < self.cleaned_data['unit']:
+                raise ValidationError(
+                    _('Issuing more than %(value)s units is not possible for date %(date)s'),
+                    params={'value': summary_for_date.stockEnd, 'date': form_date},
+                )
+
+        return self.cleaned_data['unit']
 
     class Meta:
         model = Issue
-        fields = ['product', 'unit', 'date', 'receipt_no', 'customer']
+        fields = ['product', 'date', 'unit', 'receipt_no', 'customer']
--- operation/models/daily_summary_model.py
+++ operation/models/daily_summary_model.py
@@ -6,6 +6,20 @@ from django.utils.translation import ugettext_lazy as _
 from catalogue.models import Product
 
 
+class DailySummaryManager(models.Manager):
+    def has_previous_summary(self, product, form_date):
+        return self.filter(product=product, date__lte=form_date).exists()
+
+    def get_summary_for_date(self, product, form_date):
+        obj, created = self.get_or_create(product=product,date=form_date)
+
+        if created:
+            last_summary = self.filter(product=product, date__lt=form_date).order_by('date').last()
+            obj.stockStart = last_summary.stockEnd
+            obj.stockEnd = last_summary.stockEnd
+            obj.save()
+        return obj
+
 class DailySummary(CodedBase):
     _prefix = 'SUM'
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
@@ -15,6 +29,7 @@ class DailySummary(CodedBase):
     totalIssued = models.IntegerField(default=0)
     totalReturned = models.IntegerField(default=0)
     date = models.DateField()
+    objects = DailySummaryManager()
 
     class Meta:
         app_label = 'operation'
