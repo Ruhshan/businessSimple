b2c0a5635a146654fa104cba30442d0b4c33f9c5
Ruhshan <ruhshan.ahmed@gmail.com>
2020-07-24 20:33:20 +0600

    Fix negative stockEnd appearance during issuing product

--- operation/forms/issue_form.py
+++ operation/forms/issue_form.py
@@ -9,13 +9,15 @@ class IssueForm(ModelForm):
         today = datetime.datetime.today().date()
         form_date = self.cleaned_data['date']
         form_product = self.cleaned_data['product']
-        has_previous_summary = DailySummary.objects.has_previous_summary(form_product, form_date)
 
         if today < form_date:
             raise ValidationError("Future date is not allowed")
 
+        has_previous_summary = DailySummary.objects.has_previous_summary(form_product, form_date)
+
         if not has_previous_summary:
             raise ValidationError("No product in stock for this date")
+
         return form_date
 
     def clean_unit(self):
@@ -23,11 +25,17 @@ class IssueForm(ModelForm):
             form_date = self.cleaned_data['date']
             form_product = self.cleaned_data['product']
             summary_for_date = DailySummary.objects.get_summary_for_date(form_product, form_date)
-            if summary_for_date.stockEnd < self.cleaned_data['unit']:
+            form_unit = self.cleaned_data['unit']
+            if summary_for_date.stockEnd < form_unit:
                 raise ValidationError(
                     _('Issuing more than %(value)s units is not possible for date %(date)s'),
                     params={'value': summary_for_date.stockEnd, 'date': form_date},
                 )
+            summaries_after_date = DailySummary.objects.get_summary_after_date(form_product, form_date)
+            for obj in summaries_after_date:
+                if obj.stockEnd < form_unit:
+                    raise ValidationError(_('Issuing %(unit)s will make stocks negative on %(date)s'),
+                                          params={'unit':form_unit,'date':form_date})
 
         return self.cleaned_data['unit']
 
